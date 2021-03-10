ff1d9ec1a2c4c0a371db94155d227d1aad34334b
ssnio <saeed.salehi.n@gmail.com>
2021-03-10 08:55:16 +0100

    solutions to W8T2

---
+++ tutorials/W8_AutoEncoders_GANs/solutions/W8_Tutorial2_Solution_Ex63.py
@@ -0,0 +1,16 @@
+def get_cycle_consistency_loss(real_X, fake_Y, gen_YX, cycle_criterion):
+    '''
+    Return the cycle consistency loss of the generator given inputs
+    (and the generated images for testing purposes).
+    Parameters:
+        real_X: the real images from pile X
+        fake_Y: the generated images of class Y
+        gen_YX: the generator for class Y to X; takes images and returns the images 
+            transformed to class X
+        cycle_criterion: the cycle consistency loss function; takes the real images from X and
+                        those images put through a X->Y generator and then Y->X generator
+                        and returns the cycle consistency loss (which you aim to minimize)
+    '''
+    cycle_X = gen_YX(fake_Y)
+    cycle_loss = cycle_criterion(cycle_X, real_X)
+    return cycle_loss, cycle_X
