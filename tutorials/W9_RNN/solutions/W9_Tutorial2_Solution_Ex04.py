b4921aeb0768b874e51b6bb0b9c33d2744ed9bb2
Saeed Salehi <saeed.salehi.n@gmail.com>
2021-03-17 08:58:22 +0100

    W9T2 solutions added

---
+++ tutorials/W9_RNN/solutions/W9_Tutorial2_Solution_Ex04.py
@@ -0,0 +1,28 @@
+def evaluateMultinomial(prime_str, predict_len, temperature=0.8):
+    hidden = decoder.init_hidden()
+    predicted = prime_str
+
+    # "Building up" the hidden state
+    for p in range(len(prime_str) - 1):
+        inp = char_tensor(prime_str[p])
+        _, hidden = decoder(inp, hidden)
+    
+    # Tensorize of the last character
+    inp = char_tensor(prime_str[-1])
+    
+    # For every index to predict
+    for p in range(predict_len):
+
+        # Pass the character + previous hidden state to the model
+        output, hidden = decoder(inp, hidden)
+        
+        # Sample from the network as a multinomial distribution
+        output_dist = output.data.view(-1).div(temperature).exp()
+        top_i = torch.multinomial(output_dist, 1)[0]
+        
+        # Add predicted character to string and use as next input
+        predicted_char = all_characters[top_i]
+        predicted += predicted_char
+        inp = char_tensor(predicted_char)
+
+    return predicted
---
+++ tutorials/W9_RNN/solutions/W9_Tutorial2_Solution_Ex06.py
@@ -0,0 +1,5 @@
+def truncate_pad(line, num_steps, padding_token):
+    """Truncate or pad sequences."""
+    if len(line) > num_steps:
+        return line[:num_steps]  # Truncate
+    return line + [padding_token] * (num_steps - len(line))  # Pad
