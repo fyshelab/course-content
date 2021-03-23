663e18c434a5f67309202a8c068f603c003ec6d3
anushreehede <shuttler22@gmail.com>
2021-03-22 20:27:51 -0400

    Update W9_Tutorial2_Solution_Ex03.py

--- tutorials/W9_RNN/solutions/W9_Tutorial2_Solution_Ex03.py
+++ tutorials/W9_RNN/solutions/W9_Tutorial2_Solution_Ex03.py
@@ -1,11 +1,11 @@
-def evaluate(prime_str, predict_len):
-    hidden = decoder.init_hidden()
+def evaluate(net, prime_str, predict_len):
+    hidden = net.init_hidden()
     predicted = prime_str
 
     # "Building up" the hidden state
     for p in range(len(prime_str) - 1):
         inp = char_tensor(prime_str[p])
-        _, hidden = decoder(inp, hidden)
+        _, hidden = net(inp, hidden)
     
     # Tensorize of the last character
     inp = char_tensor(prime_str[-1])
@@ -13,8 +13,8 @@ def evaluate(prime_str, predict_len):
     # For every index to predict
     for p in range(predict_len):
         
-        # Pass the character + previous hidden state to the model
-        output, hidden = decoder(inp, hidden)
+        # Pass the inputs to the model
+        output, hidden = net(inp, hidden)
         
         # Pick the character with the highest probability 
         top_i = torch.argmax(torch.softmax(output, dim=1))
