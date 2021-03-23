9a8f076b63b57ec374ab6e52f088ac89f85098c5
anushreehede <shuttler22@gmail.com>
2021-03-22 20:29:21 -0400

    Update W9_Tutorial2_Solution_Ex06.py

--- tutorials/W9_RNN/solutions/W9_Tutorial2_Solution_Ex06.py
+++ tutorials/W9_RNN/solutions/W9_Tutorial2_Solution_Ex06.py
@@ -1,5 +1,11 @@
 def truncate_pad(line, num_steps, padding_token):
     """Truncate or pad sequences."""
+    # Truncate
     if len(line) > num_steps:
-        return line[:num_steps]  # Truncate
-    return line + [padding_token] * (num_steps - len(line))  # Pad
+        return line[:num_steps]
+
+    # Pad
+    number_of_pad_tokens = num_steps - len(line)
+    padding_list = [padding_token] * number_of_pad_tokens
+
+    return line + padding_list
