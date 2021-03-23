6bf913bd38f28666d8886c99cf017d66850570d5
anushreehede <shuttler22@gmail.com>
2021-03-22 20:27:23 -0400

    Update W9_Tutorial2_Solution_Ex02.py

--- tutorials/W9_RNN/solutions/W9_Tutorial2_Solution_Ex02.py
+++ tutorials/W9_RNN/solutions/W9_Tutorial2_Solution_Ex02.py
@@ -1,26 +1,27 @@
-class biRNN(nn.Module):
-  def __init__(self, batch_size, output_size, hidden_size, vocab_size, embedding_length):
-    super(biRNN, self).__init__()
+class biLSTM(nn.Module):
+  def __init__(self, output_size, hidden_size, vocab_size, embed_size):
+    super(biLSTM, self).__init__()
 
-    self.batch_size = batch_size
     self.output_size = output_size
     self.hidden_size = hidden_size
     self.vocab_size = vocab_size
-    self.embedding_length = embedding_length
+    self.embed_size = embed_size
 
-    self.word_embeddings = nn.Embedding(vocab_size, embedding_length)
+    self.word_embeddings = nn.Embedding(vocab_size, embed_size)
     self.dropout = nn.Dropout(0.5)
-    self.rnn = nn.RNN(embedding_length, hidden_size, num_layers=2, bidirectional=True)
+    self.bilstm = nn.LSTM(embed_size, hidden_size, num_layers=2, bidirectional=True)
     self.fc = nn.Linear(4*hidden_size, output_size)
 
-  def forward(self, input_sentences, batch_size=None):
-    input = self.word_embeddings(input_sentences)
-    input = input.permute(1, 0, 2)
-    h_0 =  Variable(torch.zeros(4, input.size()[1], self.hidden_size).to(device))
+  def forward(self, input_sentences):
+
+    input = self.word_embeddings(input_sentences).permute(1, 0, 2)
+    hidden = (torch.randn(4, input.shape[1], self.hidden_size).to(device),
+            torch.randn(4, input.shape[1], self.hidden_size).to(device))
     input = self.dropout(input)
-    output, h_n = self.rnn(input, h_0)
-    h_n = h_n.permute(1, 0, 2) 
-    h_n = h_n.contiguous().view(h_n.size()[0], h_n.size()[1]*h_n.size()[2])
-    logits = self.fc(h_n)
     
+    output, hidden = self.bilstm(input, hidden)
+
+    h_n = hidden[0].permute(1, 0, 2)
+    h_n = h_n.contiguous().view(h_n.shape[0], -1)
+    logits = self.fc(h_n)
     return logits
