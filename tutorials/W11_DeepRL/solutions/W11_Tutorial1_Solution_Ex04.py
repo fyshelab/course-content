b5e5125a52a5c1de620512b6b84480dcd90dc118
Saeed Salehi <49148519+ssnio@users.noreply.github.com>
2021-03-29 14:35:47 +0200

    Update W11_Tutorial1_Solution_Ex04.py

--- tutorials/W11_DeepRL/solutions/W11_Tutorial1_Solution_Ex04.py
+++ tutorials/W11_DeepRL/solutions/W11_Tutorial1_Solution_Ex04.py
@@ -1,5 +1,19 @@
 # Q-learning
 def q_learning_backup(state, action, reward, next_state, values, params):
+    '''
+    Compute a new set of q-values using the q-learning update rule.
+    Args:
+        state (tuple): s_t, a tuple of xy coordinates.
+        action (int): a_t, an integer from {0, 1, 2, 3}.
+        reward (float): the reward of executing a_t at s_t.
+        next_state (tuple): s_{t+1}, a tuple of xy coordinates.
+        values (ndarray): an (h, w, 4) numpy array of q-values. values[y, x, a] 
+                          stores the value of executing action a at state (x, y).
+        params (dict): a dictionary of parameters.
+
+    Returns:
+        ndarray: the updated q-values.
+    '''
     x, y = state
     nx, ny = next_state
     gamma = params['gamma']
