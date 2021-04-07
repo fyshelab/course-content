38013cdd510883abf0a6b170bcdc62541ffabbf0
Saeed Salehi <49148519+ssnio@users.noreply.github.com>
2021-04-07 09:40:15 +0200

    Update W11_Tutorial2_Solution_Ex04.py

--- tutorials/W11_DeepRL/solutions/W11_Tutorial2_Solution_Ex04.py
+++ tutorials/W11_DeepRL/solutions/W11_Tutorial2_Solution_Ex04.py
@@ -7,19 +7,33 @@ class DoubleDQNAgent(DQNAgent):
       return
     elif frame == self.train_start:
       self.is_waiting = False
+
     # Sample a batch of steps for training
     batch = self.replay_buffer.sample(self.batch_size)
 
-    # Compute our predicted q-value given the state and action from our batch
-    q_pred = self.q_net(batch.state).gather(1, batch.action)
-    # Now compute the bellman backup using the target network and q-network
-    # First get the best actions from the q-network and the next state
-    max_acts = self.q_net(batch.next_state).argmax(dim=1).view(-1,1)
-    # Next get the q-values from the target network using these actions
-    # q_target = self.target_net(batch.next_state)[range(len(max_acts)), max_acts].view(-1, 1)
-    q_target = self.target_net(batch.next_state).gather(1, max_acts)
-    # Now multiply by the discount and add the reward signal
-    q_target = batch.reward + batch.discount * q_target.detach()
+    # First let's compute our predicted q-values
+    # We need to pass our batch of states (batch.state) to our q_net    
+    q_actions = self.q_net(batch.state)
+    # Then we select the q-values that correspond to the actions in our batch
+    # (batch.action) to get our predictions (hint: use the gather method)
+    q_pred = q_actions.gather(1, batch.action)
+
+    # Now compute the q-value target (also known as the td target or bellman
+    # backup) using our target network. Since we don't need gradients for this,
+    # we disable autograd here to speed up performance
+    with torch.no_grad():
+      # Compute the action values from our q_net using a batch.next_state
+      q_next_actions = self.q_net(batch.next_state)
+      # Use this to find the actions that correspond to the largest values
+      # (i.e. argmax)
+      max_acts = q_next_actions.argmax(dim=1).view(-1,1)
+      # Next get the action values using our target_net and batch.next_state
+      q_target_actions = self.target_net(batch.next_state)
+      # Then we select the q-values that correspond to the actions we just found
+      # (hint: use the gather method)
+      q_target = q_target_actions.gather(1, max_acts)
+      # Next multiply by batch.discount and add batch.reward
+      q_target = batch.reward + batch.discount * q_target
     # Compute the MSE loss between the predicted and target values, then average
     # over the batch
     loss = F.mse_loss(q_pred, q_target).mean()    
